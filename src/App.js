fb8ea4edf8c331f9ff9ff8301978c8f5625d96a7
nurmdrafi <nurmdrafi2014@gmail.com>
2022-03-31 18:21:40 +0600

    implement router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 // import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Inventory from "./components/Inventory/Inventory";
 import Shop from "./components/Shop/Shop";
+import Orders from "./components/Orders/Orders";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route path="/" element={<Shop></Shop>}></Route>
+        <Route path="/shop" element={<Shop></Shop>}></Route>
+        <Route path="/orders" element={<Orders></Orders>}></Route>
+        <Route path="/inventory" element={<Inventory></Inventory>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
