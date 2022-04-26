ea0ac2d0653a46ac17f9e96d73196c99dbae0bbd
nurmdrafi <nurmdrafi2014@gmail.com>
2022-04-27 01:27:32 +0600

    implement get method

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,33 @@ const port = process.env.POST || 5000;
 app.use(cors()); // It allows us to relax the security applied to an API
 app.use(express.json()); // For parsing body of POST and PUT Method
 
-// for testing
-app.get("/", (req, res) => {
-  res.send({ message: "Success" });
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sw4ok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("emaJohn").collection("products");
+
+    // get all data
+    app.get("/products", async (req, res) => {
+      const query = req.query;
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      console.log(result);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("Listening to port", port);
